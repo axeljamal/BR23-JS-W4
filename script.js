3f6b45f83f9832520a6464b570df82400904ee45
Axel Jamal <axeltaj@gmail.com>
2023-01-10 12:01:13 +0000

    updated solution

--- script.js
+++ script.js
@@ -1,36 +1,43 @@
-let choices = ["r", "p", "s"];
-let win = 0
-let losses = 0
-let ties = 0
-let usersChoice = prompt("Choose r, p or s");
+// Creates an array that lists out all of the options (Rock, Paper, or Scissors).
+var computerChoices = ["r", "p", "s"];
 
-let computerChoice = choices[Math.floor(Math.random() * choices.length)]
+// Creating variables to hold the number of wins, losses, and ties. They start at 0.
+var wins = 0;
+var losses = 0;
+var ties = 0;
 
-if (usersChoice === "r" && computerChoice === "s") {
-    alert("You win!");
-    win = win + 1;
-} else if (usersChoice === "r" && computerChoice === "p") {
-    alert("You lose!");
-    losses = losses + 1;
-} if (usersChoice === "s" && computerChoice === "r") {
-    alert("You lose!");
-    losses = losses + 1;
-} if (usersChoice === "s" && computerChoice === "p") {
-    alert("You win!");
-    win = win + 1;
-} if (usersChoice === "p" && computerChoice === "r") {
-    alert("You win!");
-    win = win + 1;
-} if (usersChoice === "p" && computerChoice === "s") {
-    alert("You lose!");
-    losses = losses + 1;
-} if (usersChoice === computerChoice) {
-    alert("It's a tie");
-    ties = ties + 1;
+for (var i = 0; i < 10; i++) {
+  // Randomly chooses a choice from the options array. This is the Computer's guess.
+  var computerGuess =
+    computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+  // Collect the user's response and convert to lower case.
+  var userGuess = prompt("Enter r, p, or s to play!");
+  userGuess = userGuess.toLowerCase();
+
+  // Only run game logic if user chose a valid option
+  if (userGuess === "r" || userGuess === "p" || userGuess === "s") {
+    alert("The computer chose " + computerGuess);
+
+    // Win/lose conditions:
+    if (userGuess === computerGuess) {
+      ties++;
+      alert("You've tied " + ties + " time(s).");
+    } else if (
+      (userGuess === "r" && computerGuess === "s") ||
+      (userGuess === "s" && computerGuess === "p") ||
+      (userGuess === "p" && computerGuess === "r")
+    ) {
+      wins++;
+      alert("You've won " + wins + " time(s)!");
+    } else {
+      losses++;
+      alert("You've lost " + losses + " time(s).");
+    }
+  }
 }
 
-let playAgain = confirm("Would you like to play again?")
-
-while (playAgain === true) {
-    function compare("r", "p", "s");
-} 
+// When the game is over, alert the totals to the user. We can use the \n character to make a line break.
+alert(
+  "Total wins: " + wins + "\nTotal ties: " + ties + "\nTotal losses: " + losses
+);
